7b2957b68eafef6de4d35645cc47e49627e0cd52
Juan Carlos Perez Angeles <juan-carlos-perez-@outlook.com>
2022-05-31 12:22:37 -0500

    Implementación de métodos GET y POST

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const Peliculas = require('../config/esquemas');
+require('../config/conexion');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  Peliculas.find().then(result => {
+    res.status(200);
+    res.render('index', {result})
+  });
+});
+
+router.delete('/:id', (req, res, next) => {
+  var route = "/";
+  Peliculas.deleteOne({'_id': req.params.id}).then(result => {
+    res.status(200);
+    res.redirect(route);
+  });
 });
 
 module.exports = router;
