7f6d3890fef3c4eded077b8eeb5230946825929c
jorgejosht <jorgejoshuatrejo@hotmail.com>
2022-06-01 13:09:35 -0500

    Implementación método PUT y correciones

--- routes/index.js
+++ routes/index.js
@@ -36,6 +36,33 @@ router.post('/addform', (req, res, next) => {
   res.redirect(route);
 });
 
+router.put('/updateform/:id', (req, res, next) => {
+  var route = "/";
+  Peliculas.findByIdAndUpdate({'_id': req.params.id}, {
+      id: req.params.id,
+      titulo: req.body.titulo,
+      anno: req.body.anno,
+      genero: req.body.genero,
+      duracion: req.body.duracion,
+      sinopsis: req.body.sinopsis,
+      director: req.body.director,
+      reparto: req.body.reparto
+  }, function (err, result) {
+      if (err) {
+          console.log(err)
+      } else {
+          res.status(200)
+          res.redirect(route);
+      }
+  });
+});
+
+router.get('/updateform/:id', (req, res, next) => {
+  Peliculas.findOne({'_id': req.params.id}).then(result => {
+      res.render('updateform', {result});
+  });
+});
+
 router.get('/addform', function(req, res, next) {
   Peliculas.find().then(result => {
     res.status(200);
